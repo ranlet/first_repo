a02961cfc03587f8435360a843672ffb3155a36f
ranlet <dmitry.redichkin2000@gmail.com>
2024-01-29 12:25:18 +0300

    4 задача

--- 4.py
+++ 4.py
@@ -1,1 +1,8 @@
-
+import csv
+with open('first_repo/students.csv', encoding="utf8") as a:
+    reader = list(csv.DictReader(a, delimiter=',', quotechar='"'))
+    arr = list(reader)
+a = {}
+print ('id,Name,titleProject_id,class,score')
+for i in arr:
+    print(i['id'] + " " + i['Name'] + ' ' + i['titleProject_id'] + " " + i['class'] + " " + i['score'])
