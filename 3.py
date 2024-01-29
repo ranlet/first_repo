3fbb3e4db04e8c5f1089f93e3c3662201e942ab8
ranlet <dmitry.redichkin2000@gmail.com>
2024-01-29 12:16:03 +0300

    3 задача

--- 3.py
+++ 3.py
@@ -1,11 +1,20 @@
 import csv
+c = input()
 with open('first_repo/students.csv', encoding="utf8") as a:
     reader = list(csv.DictReader(a, delimiter=',', quotechar='"'))
     arr = list(reader)
-    print(arr)
 a = {}
+flag = False
 for i in arr:
-    f = i['class']
-    if '10' in f:
+    f = i['titleProject_id']
+    if c == f:
+        flag = True
         d = {i['id']:i['score']}
-        print(d)
+        name = i['Name']
+        SecName = name.split()
+        second_name = SecName[0]
+        fir_name = SecName[1]
+        first_name = fir_name[0] + '.'
+        print("Проект №" + c + " делал: " + first_name + ' ' + second_name + " он(а) получил(а) оценку - " + i['score'])
+if not flag:
+    print('Ничего не найдено.')
